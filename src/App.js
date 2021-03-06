3a78c8b4880e04b68bdf0b04cdd20ee709634f97
parispoly137 <tkdgns6578@naver.com>
2021-03-06 17:35:24 +0900

    #3 STATE
    * The concept of state and how to write.
    * Efficient approach to state (current)
    * The main method related to the life cycle of a component.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,48 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h3>I like {name}!!</h3>
-      <h5>{rating}/5.0</h5>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 4
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 3.7
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.2
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 4.6
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.8
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
   }
-];
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  );
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  }
+
+  constructor(props) {
+    super(props);
+    console.log("I just got born.");
+  }
+
+  componentDidMount() {
+    console.log("Rendered for the first time.")
+  }
+
+  componentDidUpdate() {
+    console.log("The component has been updated.")
+  }
+
+  componentWillUnmount() {
+    console.log("The component is dead.") //But you won't be able to see it.
+  }
+
+  render() {
+    console.log("Rendering complete.");
+    return (
+      <div>
+        <h1>The number is: {this.state.count} </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+
+    )
+  }
+
 }
 
 export default App;
