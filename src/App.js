7b1fc182483077890c471e1386674e3e3a3a5bad
parispoly137 <tkdgns6578@naver.com>
2021-03-08 14:46:44 +0900

    #4.0 Fetching Movies from API
    - axios를 이용하여 데이터를 가져옴.
    - 비동기 함수이므로 async와 await를 이용한다.

--- src/App.js
+++ src/App.js
@@ -1,75 +1,26 @@
-import React from 'react';
-import PropTypes from "prop-types"
-
-let friends = [
-  {
-    id: 1,
-    name: "Jason",
-    age: 15
-  },
-  {
-    id: 2,
-    name: "Jackson",
-    age: 19
-  },
-  {
-    id: 3,
-    name: "Samuel",
-    age: 23
-  }
-];
-
-
-
-function Friends({ name, age }) {
-  return (
-    <h5>I wanna be friends with {name} who is {age} years old.</h5>
-  )
-}
-
-Friends.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired
-}
-
-
+import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movie: [],
-    count: 0
-  }
+    movie: []
+  };
+
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    console.log(movies)
+  };
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 3000);
-  }
-
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  }
-
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  }
+    this.getMovies();
+  };
 
   render() {
     const { isLoading } = this.state;
 
     return (
-      <div>
-        <h1>{isLoading ? "Loading..." : "We are ready!!"}</h1>
-        <div>
-          <h2>The number is: {this.state.count}</h2>
-          <button onClick={this.add}>Add</button>
-          <button onClick={this.minus}>Minus</button>
-        </div>
-        {friends.map(friend => (
-          <Friends key={friend.id} name={friend.name} age={friend.age} />
-        ))}
-      </div>
+      <h1>{isLoading ? "Loading..." : "We are ready!!"}</h1>
     )
   }
 }
