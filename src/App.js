2dd50c1ae409b710c08b5768b5a06cc8aa3cf505
parispoly137 <tkdgns6578@naver.com>
2021-03-08 15:34:01 +0900

    #4.1 Rendering the Movies
    state에 저장된 movie들의 정보를 렌더링.
    Movie.js 파일을 따로 만들어서 props를 설정하고, App.js에서 props에 해당하는 정보를 보낸다.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movie: []
+    movies: []
   };
 
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
-    console.log(movies)
+
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount() {
@@ -17,10 +19,14 @@ class App extends React.Component {
   };
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
 
     return (
-      <h1>{isLoading ? "Loading..." : "We are ready!!"}</h1>
+      <h5>{isLoading ? "Loading..." : movies.map(movie => {
+        return (
+          <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+        )
+      })}</h5>
     )
   }
 }
