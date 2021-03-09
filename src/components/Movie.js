3de0b1dfeb1c35b0ee7cabb4da5b074e20cdca1b
parispoly137 <tkdgns6578@naver.com>
2021-03-09 22:11:05 +0900

    #6.0 Getting Ready for the Router
    - react router dom 설치
      ~ 네비게이션 추가 위함
    - router를 위해 폴더 추가 및 코드 이동
      ~ (App.js -> Home.js)
    - import 경로 수정

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./Movie.css";
+import "./components/Movie.css";
 
 function Movie({ year, title, genres, summary, poster }) {
   return (
@@ -9,15 +9,13 @@ function Movie({ year, title, genres, summary, poster }) {
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="movie__genres">{genres.map((genre, index) => {
-          return (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          )
-        })}</ul>
-        <p className="movie__summary">{summary.slice(0, 180)}...
-</p>
+        <ul className="movie__genres">{genres.map((genre, index) =>
+          <li key={index} className="genres__genre">
+            {genre}
+          </li>
+        )}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
 
     </div>
